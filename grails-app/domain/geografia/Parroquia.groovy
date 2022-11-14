b402c25029973f183cfed858c52bac6c58c3cb33
Guido Ochoa <guido8a@gmail.com>
2022-11-14 14:00:50 -0500

    Parroquias

---
+++ grails-app/domain/geografia/Parroquia.groovy
@@ -0,0 +1,72 @@
+package geografia
+
+/**
+ * Clase para conectar con la tabla 'parr' de la base de datos<br/>
+ * Guarda la lista de parroquias por cantón
+ */
+class Parroquia {
+    /**
+     * Cantón al cual pertenece la parroquia
+     */
+    Canton canton
+    /**
+     * Número de parroquia
+     */
+    Integer numero
+    /**
+     * Nombre de la parroquia
+     */
+    String nombre
+    /**
+     * Código de la parroquia
+     */
+    String codigo
+
+    /**
+     * Posición geográfica de la parroquia (longitud para ubicar en un mapa)
+     */
+    double longitud
+    /**
+     * Posición geográfica de la parroquia (latitud para ubicar en un mapa)
+     */
+    double latitud
+
+    /**
+     * Define el mapeo entre los campos del dominio y las columnas de la base de datos
+     */
+    static mapping = {
+        table 'parr'
+        cache usage: 'read-write', include: 'non-lazy'
+        id column: 'parr__id'
+        id generator: 'identity'
+        version false
+        columns {
+            id column: 'parr__id'
+            canton column: 'cntn__id'
+            numero column: 'parrnmro'
+            nombre column: 'parrnmbr'
+            codigo column: 'parrcdgo'
+
+            longitud column: 'parrlong'
+            latitud column: 'parrlatt'
+        }
+    }
+
+    /**
+     * Define las restricciones de cada uno de los campos
+     */
+    static constraints = {
+        canton(blank: true, nullable: true, attributes: [mensaje: 'Cantón al que pertenece la parroquia'])
+        numero(blank: true, nullable: true, attributes: [mensaje: 'Número de la parroquia'])
+        nombre(size: 1..63, blank: true, nullable: true, attributes: [mensaje: 'Nombre de la parroquia'])
+        codigo(size: 1..6, blank: true, nullable: true, attributes: [mensaje: 'Código de la parroquia'])
+    }
+
+    /**
+     * Genera un string para mostrar
+     * @return el nombre
+     */
+    String toString() {
+        return this.nombre
+    }
+}
