340ef3124c59b2cd35b46bd365e4a710b1a75c1e
Guido Ochoa <guido8a@gmail.com>
2022-11-15 14:19:49 -0500

    Dominios adicionales

---
+++ grails-app/domain/rikcharina/ObrasFinca.groovy
@@ -0,0 +1,35 @@
+package rikcharina
+
+class ObrasFinca {
+
+    static auditable = true
+
+    Finca    finca
+    TipoObra  tipoObra
+    String   estado
+
+    static mapping = {
+        table 'obfn'
+        cache usage: 'read-write', include: 'non-lazy'
+        version false
+        id generator: 'identity'
+        control sort: ['ctrlNombre': 'asc']
+        columns {
+            id        column: 'obfn__id'
+            finca     column: 'fnca__id'
+            tipoObra  columm: 'tpob__id'
+            estado    column: 'trfmtipo'
+        }
+    }
+
+
+    static constraints = {
+        finca (blank: false, nullable: false)
+        tipoObra (blank: false, nullable: false)
+        estado (blank: false, nullable: false)
+    }
+
+    String toString() {
+        "${this.actividad}"
+    }
+}
